20922600b0abd908abb18f1d32628326b7cf37b7
BZR4 <BZR4@users.noreply.github.com>
2026-03-12 19:42:34 -0300

    Menu

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -10,6 +10,20 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('Home Page')));
+    return Scaffold(
+      appBar: AppBar(title: Text('Home Page')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            FilledButton(onPressed: () {}, child: Text('Xylophone')),
+            FilledButton(onPressed: () {}, child: Text('Funny Sounds')),
+            FilledButton(onPressed: () {}, child: Text('Cotação Moedas')),
+          ],
+        ),
+      ),
+    );
   }
 }
