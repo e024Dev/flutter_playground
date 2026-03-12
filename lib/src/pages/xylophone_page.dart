c098f5d26f722d6b5695077bf29bcf820b5eb571
BZR4 <BZR4@users.noreply.github.com>
2026-03-12 20:37:42 -0300

    Layout Xylophone

--- lib/src/pages/xylophone_page.dart
+++ lib/src/pages/xylophone_page.dart
@@ -11,8 +11,48 @@ class _XylophonePageState extends State<XylophonePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xFF333333),
       appBar: AppBar(title: Text('Xylophone')),
-      body: Center(child: Text('body')),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          XylophoneButton(soundNumber: 1, color: Colors.red),
+          XylophoneButton(soundNumber: 2, color: Colors.orange),
+          XylophoneButton(soundNumber: 3, color: Colors.green),
+          XylophoneButton(soundNumber: 4, color: Colors.yellow),
+          XylophoneButton(soundNumber: 5, color: Colors.blue),
+          XylophoneButton(soundNumber: 6, color: Colors.purple),
+          XylophoneButton(soundNumber: 7, color: Colors.indigo),
+        ],
+      ),
+    );
+  }
+}
+
+class XylophoneButton extends StatelessWidget {
+  const XylophoneButton({
+    super.key,
+    required this.color,
+    required this.soundNumber,
+  });
+
+  final MaterialColor color;
+  final int soundNumber;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: EdgeInsets.symmetric(
+          vertical: 4.0,
+          horizontal: soundNumber * 5,
+        ),
+        child: FilledButton(
+          style: FilledButton.styleFrom(backgroundColor: color.shade400),
+          onPressed: () {},
+          child: null,
+        ),
+      ),
     );
   }
 }
