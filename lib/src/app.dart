06b9f4586207a10bfb8f70f981f432df14228912
BZR4 <BZR4@users.noreply.github.com>
2026-03-12 19:26:06 -0300

    Initial Commit

---
+++ lib/src/app.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_playground/src/pages/home_page.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: HomePage());
+  }
+}
